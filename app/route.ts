e55f10ae3d861c1c1bbc93fe510bc55be87be67f
JoiT <joit@tako.so>
2024-03-05 21:31:54 +0800

    test pure frame and only post method

--- app/route.ts
+++ app/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest } from 'next/server';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
-export function GET(request: NextRequest) {
-  const html = `
+
+export function POST(request: NextRequest) {
+  const defaultHtml = `
     <!DOCTYPE>
     <html>
       <head>
@@ -11,20 +11,17 @@ export function GET(request: NextRequest) {
         <meta property="fc:frame:image" content="${ request.nextUrl.origin }/bg.jpg" />
       </head>
       <body>
-        <!--
         <h1>Test Frame from vercel Function</h1>
         <figure>
           <img width="600" src="${ request.nextUrl.origin }/bg.jpg" />
         </figure>
-        -->
       </body>
     </html>
-  ` 
-
+  `
   console.log('request: ', request.nextUrl.host)
 
   return new Response(
-    html,
+    defaultHtml,
     {
       status: 200,
       headers: { 

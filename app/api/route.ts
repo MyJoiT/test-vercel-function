cb2a3ee2bffbdb9f6a8dc2599bbb5a70f4884f4d
JoiT <joit@tako.so>
2024-03-05 15:16:46 +0800

    add body content

--- app/api/route.ts
+++ app/api/route.ts
@@ -9,7 +9,10 @@ export function GET(request: Request) {
         <meta property="fc:frame:image" content="${ request.nextUrl.host }/bg.jpg" />
       </head>
       <body>
-        <div style="color:red">Hello World</div>
+        <h1>Test Frame from vercel Function</h1>
+          <figure>
+            <img width="600" src="${ request.nextUrl.host }/bg.jpg" />
+          </figure>
       </body>
     </html>
   ` 

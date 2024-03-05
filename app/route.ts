7c681d7e998c028507b754423c1e335d449221ed
JoiT <joit@tako.so>
2024-03-05 22:15:37 +0800

    add result html

--- app/route.ts
+++ app/route.ts
@@ -2,26 +2,70 @@ import { NextRequest } from 'next/server';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
-export function POST(request: NextRequest) {
-  const defaultHtml = `
+const defaultHtml = (host: string) => `
     <!DOCTYPE>
     <html>
       <head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${ request.nextUrl.origin }/bg.jpg" />
+        <meta property="fc:frame:image" content="${ host }/bg.jpg" />
+        <meta property="fc:frame:button:1" content="View Result" />
+        <meta property="fc:frame:post_url" content="${ host }?frame=result" />
       </head>
+
+      <!--
       <body>
         <h1>Test Frame from vercel Function</h1>
         <figure>
-          <img width="600" src="${ request.nextUrl.origin }/bg.jpg" />
+          <img width="600" src="${ host }/bg.jpg" />
         </figure>
       </body>
+      -->
     </html>
   `
-  console.log('request: ', request.nextUrl.host)
+
+const resultHtml = (host: string) => `
+    <!DOCTYPE>
+    <html>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${ host }/bg1.jpg" />
+        <meta property="fc:frame:button:1" content="Return" />
+        <meta property="fc:frame:post_url" content="${ host }?frame=default" />
+      </head>
+
+      <!--
+      <body>
+      </body>
+      -->
+    </html>
+  `
+
+export function GET(request: NextRequest) {
+  const html = defaultHtml(request.nextUrl.host)
 
   return new Response(
-    defaultHtml,
+    html,
+    {
+      status: 200,
+      headers: { 
+        'Content-Type': 'text/html; charset=utf-8' 
+      },
+    }
+  );
+}
+
+export function POST(request: NextRequest) {
+  let html = null
+
+  const searchParams = request.nextUrl.searchParams
+  if(searchParams && searchParams.frame === 'result') {
+    html = resultHtml(request.nextUrl.origin)
+  } else {
+    html = defaultHtml(request.nextUrl.origin)
+  }
+
+  return new Response(
+    html,
     {
       status: 200,
       headers: { 

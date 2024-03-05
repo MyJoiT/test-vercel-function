44db67c5831d2e53af7948159cd77ddb49552523
JoiT <joit@tako.so>
2024-03-05 15:24:01 +0800

    change request to NextRequest

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,23 +1,25 @@
+import { NextRequest } from 'next/server';
+
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
-export function GET(request: Request) {
+export function GET(request: NextRequest) {
   const html = `
     <!DOCTYPE>
     <html>
       <head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${ request.nextUrl.host }/bg.jpg" />
+        <meta property="fc:frame:image" content="${ request.nextUrl.origin }/bg.jpg" />
       </head>
       <body>
         <h1>Test Frame from vercel Function</h1>
           <figure>
-            <img width="600" src="${ request.nextUrl.host }/bg.jpg" />
+            <img width="600" src="${ request.nextUrl.origin }/bg.jpg" />
           </figure>
       </body>
     </html>
   ` 
 
-  // console.log('request: ', request.nextUrl.host)
+  console.log('request: ', request.nextUrl.host)
 
   return new Response(
     html,
